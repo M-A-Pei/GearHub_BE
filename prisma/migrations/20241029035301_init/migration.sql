b9ccd8873287658b4675ce5a7316ddf8ea33a1be
IT KOMPUTER <98211788+itkomputer01@users.noreply.github.com>
2024-11-08 22:39:34 +0700

    changed db to postgresql

--- prisma/migrations/20241029035301_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "products" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "category" TEXT NOT NULL,
-    "image" TEXT NOT NULL,
-    "price" INTEGER NOT NULL
-);
---
+++ prisma/migrations/20241108153643_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "products" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
